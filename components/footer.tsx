3b78c57155ee6b14c4a6155da36d42b27c7f6b23
NisargS28 <nisargsolanki56@gmail.com>
2026-07-16 22:25:30 +0530

    footer changed

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="pt-12 border-t border-border-dim text-[10px] flex justify-between opacity-50 uppercase tracking-widest">
-      <div>© 2024 ALEX_DEV // KERNEL: 5.15.0-LOCAL</div>
+      <div>© 2024 ALEX_Nisarg // KERNEL: 5.15.0-LOCAL</div>
       <div>LOC: 37.7749° N, 122.4194° W</div>
     </footer>
   )
